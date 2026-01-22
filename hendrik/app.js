d753e431651e465a8bbef7f1556c7016091d393c
Hendrik Tandiono <ahen.radiant@gmail.com>
2026-01-22 21:47:44 +0700

    testing qodo

---
+++ hendrik/app.js
@@ -0,0 +1,19 @@
+function greet(name){
+    return `Hello, ${name}!`;
+}
+
+function farewell(name){
+    return `Goodbye, ${name}.`;
+}
+
+function calculateSum(a, b){
+    return a + b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function calculateAge(birthYear, currentYear){
+    return currentYear - birthYear;
+}
